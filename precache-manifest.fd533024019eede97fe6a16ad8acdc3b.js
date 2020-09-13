a10d13c532fd52afef7c0ff99a12031c3bf7e552
AlexandVotinov <lolstufffff58@gmail.com>
2020-09-13 21:12:41 +0300

    Updates

---
+++ precache-manifest.fd533024019eede97fe6a16ad8acdc3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf0efcbbbc6a7c5993e7fe543e0bd8a",
+    "url": "/rates/index.html"
+  },
+  {
+    "revision": "0c932d2f889f944c38c1",
+    "url": "/rates/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "1c86b1183e03c8772bfd",
+    "url": "/rates/static/css/main.693c781a.chunk.css"
+  },
+  {
+    "revision": "0c932d2f889f944c38c1",
+    "url": "/rates/static/js/2.4ae05eb9.chunk.js"
+  },
+  {
+    "revision": "72ef779b68fc1d8289d78fa940111849",
+    "url": "/rates/static/js/2.4ae05eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c86b1183e03c8772bfd",
+    "url": "/rates/static/js/main.b0763dd0.chunk.js"
+  },
+  {
+    "revision": "2912e382c967a542d01f",
+    "url": "/rates/static/js/runtime-main.02dd40e6.js"
+  }
+]);
